ebebcc98c7ca391d8e81ac0503ae28fe952f60e7
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2019-12-25 16:15:37 -0500

    started my level

--- chapter_3.py
+++ chapter_3.py
@@ -6,16 +6,20 @@ import settings
 class Ch3View(arcade.View):
     def __init__(self):
         super().__init__()
-        self.music = arcade.Sound("End_of_Time.mp3")
-        self.music.play()
+        # self.music = arcade.Sound("End_of_Time.mp3")
+        # self.music.play()
           
     def on_show(self):
         arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Chapter 3", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        arcade.draw_rectangle_outline(200, 450, 100, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(175, 425, 50, 100, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(200, 215, 100, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(175, 240, 50, 100, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(575, 450, 100, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(600, 425, 50, 100, arcade.color.AZURE)
 
     def on_key_press(self, key, modifiers):
         self.director.next_view()
@@ -36,7 +40,7 @@ if __name__ == "__main__":
     my_view = Ch3View()
     my_view.director = FakeDirector(close_on_next_view=True)
     window.show_view(my_view)
-    music = arcade.Sound("End_of_Time.mp3")
-    music.play()
+    # music = arcade.Sound("End_of_Time.mp3")
+    # music.play()
     arcade.run()
 
