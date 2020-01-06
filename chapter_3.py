85204394d260a415bf06ff374fb3c8183c841684
Marcus <marcus.tuenmuk20@ycdsbk12.ca>
2020-01-06 14:21:36 -0500

    Created a layout of how I want my level to be

--- chapter_3.py
+++ chapter_3.py
@@ -2,26 +2,34 @@ import arcade
 
 import settings
 
-
 class Ch3View(arcade.View):
     def __init__(self):
         super().__init__()
         # self.music = arcade.Sound("End_of_Time.mp3")
         # self.music.play()
+        self.sprite_main = arcade.Sprite(center_x=400, center_y=300)
+        self.sprite_main.texture = arcade.make_circle_texture(50, arcade.color.AMAZON)
           
     def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_rectangle_outline(200, 475, 100, 50, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(175, 450, 50, 100, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(200, 125, 100, 50, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(175, 150, 50, 100, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(575, 475, 100, 50, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(600, 450, 50, 100, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(575, 125, 100, 50, arcade.color.AZURE)
-        arcade.draw_rectangle_outline(600, 150, 50, 100, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(250, 475, 150, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(200, 425, 50, 150, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(250, 125, 150, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(200, 175, 50, 150, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(525, 475, 150, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(575, 425, 50, 150, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(525, 125, 150, 50, arcade.color.AZURE)
+        arcade.draw_rectangle_outline(575, 175, 50, 150, arcade.color.AZURE)
+        arcade.draw_rectangle_filled(400, 590, 150, 20, arcade.color.ASH_GREY)
+        arcade.draw_circle_filled(50, 50, 50, arcade.color.WHITE)
+        arcade.draw_circle_filled(50, 550, 50, arcade.color.WHITE)
+        arcade.draw_circle_filled(750, 50, 50, arcade.color.WHITE)
+        arcade.draw_circle_filled(750, 550, 50, arcade.color.WHITE)
+        self.sprite_main.draw()
+        
         
     def on_key_press(self, key, modifiers):
         self.director.next_view()
