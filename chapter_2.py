4f47ead888f572713f8eeff5a92e75270fecf9cd
figoli <figo781802514@gmail.com>
2020-01-20 09:33:32 -0500

    add get/set

--- chapter_2.py
+++ chapter_2.py
@@ -7,9 +7,6 @@ Left_ViewPoint_Margin = 150
 Right_ViewPoint_Margin = 150
 Bottom_ViewPoint_Margin = 50
 Top_ViewPoint_Margin = 400
-
-WIDTH = 800
-HEIGHT = 600
 Gravity = 0.5
 
 
@@ -74,55 +71,92 @@ class Player(arcade.Sprite):
     def __init__(self, filename=None, scale=0.3, center_x=760, center_y=128):
         super().__init__(filename=filename, scale=scale, center_x=center_x, center_y=center_y)
 
-        self.Player_move_speed = 3
-        self.Player_jump_speed = 3
-        self.view_bottom = 0
-        self.view_left = 0
+        self._Player_move_speed = 3
+        self._Player_jump_speed = 3
+        self._view_bottom = 0
+        self._view_left = 0
 
-        self.coins = 0
+        self._coins = 0
         self.bag = []
         self.open_door = False
 
-        self.charge_power = 0
-        self.score = 0
+        self._charge_power = 0
+
+    def get_player_move_speed(self):
+        return self._Player_move_speed
+
+    def set_player_move_speed(self, value):
+        self._Player_move_speed = value
+
+    def get_player_jump_speed(self):
+        return self._Player_jump_speed
+
+    def set_player_jump_speed(self, value):
+        if value > 15:
+            value = 15
+        self._Player_jump_speed = value
+
+    def get_view_bottom(self):
+        return self._view_bottom
+
+    def set_view_bottom(self, value):
+        self._view_bottom = value
+
+    def get_view_left(self):
+        return self._view_left
+
+    def set_view_left(self, value):
+        self._view_left = value
+
+    def get_coins(self):
+        return self._coins
+
+    def set_coins(self, value):
+        self._coins = value
+
+    def get_charge_power(self):
+        return self._charge_power
+
+    def set_charge_power(self, value):
+        self._charge_power = value
 
     def update(self):
 
         # Create the view change when player moves
         changed = False
 
-        left_boundary = self.view_left + Left_ViewPoint_Margin
+        left_boundary = self.get_view_left() + Left_ViewPoint_Margin
         if self.left < left_boundary:
-            self.view_left -= left_boundary - self.left
+            self._view_left -= left_boundary - self.left
             changed = True
 
         # Scroll right
-        right_boundary = self.view_left + WIDTH - Right_ViewPoint_Margin
+        right_boundary = self.get_view_left() + settings.WIDTH - Right_ViewPoint_Margin
         if self.right > right_boundary:
-            self.view_left += self.right - right_boundary
+            self._view_left += self.right - right_boundary
             changed = True
 
         # Scroll up
-        top_boundary = self.view_bottom + HEIGHT - Top_ViewPoint_Margin
+        top_boundary = self.get_view_bottom() + settings.HEIGHT - Top_ViewPoint_Margin
         if self.top > top_boundary:
-            self.view_bottom += self.top - top_boundary
+            self._view_bottom += self.top - top_boundary
             changed = True
 
         # Scroll down
-        bottom_boundary = self.view_bottom + Bottom_ViewPoint_Margin
+        bottom_boundary = self.get_view_bottom() + Bottom_ViewPoint_Margin
         if self.bottom < bottom_boundary:
-            self.view_bottom -= bottom_boundary - self.bottom
+            self._view_bottom -= bottom_boundary - self.bottom
             changed = True
 
         if changed:
-            self.view_bottom = int(self.view_bottom)
-            self.view_left = int(self.view_left)
+            self.set_view_bottom(int(self.get_view_bottom()))
+            self.set_view_left(int(self.get_view_left()))
 
             # Do the scrolling
-            arcade.set_viewport(self.view_left,
-                                WIDTH + self.view_left,
-                                self.view_bottom,
-                                HEIGHT + self.view_bottom)
+            arcade.set_viewport(self.get_view_left(),
+                                settings.WIDTH + self.get_view_left(),
+                                self.get_view_bottom(),
+                                settings.HEIGHT + self.get_view_bottom())
 
     def check_bag(self):
         """
@@ -165,11 +199,10 @@ class Chapter2View(arcade.View):
 
     def __init__(self):
         super().__init__()
-        self.Total_time = 0
-        self.space_press_time = 0
-        self.button_hit_time = 0
-        self.button_press_time = 0
-        self.direction_draw_time = 0
+        self._Total_time = 0
+        self._space_press_time = 0
+        self._button_hit_time = 0
+        self._button_press_time = 0
         self.score_data = None
 
         self.up_pressed = False
@@ -197,6 +230,30 @@ class Chapter2View(arcade.View):
                                 "Collect all the coins and press 'E' in front of the terminal door\n" \
                                 "The orange button will give you direction support. Good Luck!"
 
+    def get_total_time(self):
+        return self._Total_time
+
+    def set_total_time(self, value):
+        self._Total_time = value
+
+    def get_space_press_time(self):
+        return self._space_press_time
+
+    def set_space_press_time(self, value):
+        self._space_press_time = value
+
+    def get_button_hit_time(self):
+        return self._button_hit_time
+
+    def set_button_hit_time(self, value):
+        self._button_hit_time = value
+
+    def get_button_press_time(self):
+        return self._button_press_time
+
+    def set_button_press_time(self, value):
+        self._button_press_time = value
+
     def on_show(self):
         arcade.set_background_color(arcade.color.BLUE_SAPPHIRE)
 
@@ -209,13 +266,13 @@ class Chapter2View(arcade.View):
         self.player.draw()
 
         # The number of coins
-        coin_text = f"Coins: {self.player.coins}/10"
+        coin_text = f"Coins: {self.player.get_coins()}/10"
         arcade.draw_text(coin_text, self.player.center_x, self.player.center_y + 25, arcade.csscolor.BLACK, 12)
 
         # The charge power rectangle
         arcade.draw_rectangle_outline(self.player.center_x, self.player.center_y - 50, 20, 70, arcade.color.BLACK, 3,
                                       90)
-        arcade.draw_rectangle_filled(self.player.center_x, self.player.center_y - 50, 20, self.player.charge_power,
+        arcade.draw_rectangle_filled(self.player.center_x, self.player.center_y - 50, 20, self.player.get_charge_power(),
                                      arcade.color.RED, 90)
 
         # The end screen
@@ -225,7 +282,7 @@ class Chapter2View(arcade.View):
             arcade.draw_rectangle_filled(self.player.center_x - 200, self.player.center_y + 210, 600, 300,
                                          arcade.color.WHITE)
 
-            if round(self.Total_time, 2) == self.score_data[0]:
+            if round(self.get_total_time(), 2) == self.score_data[0]:
                 arcade.draw_text(f"1 {self.score_format} {self.score_data[0]} SEC", self.player.center_x - 350,
                                  self.player.center_y + 290, arcade.csscolor.RED, 30)
             else:
@@ -233,7 +290,7 @@ class Chapter2View(arcade.View):
                                  self.player.center_y + 290, arcade.csscolor.BLACK, 30)
 
             try:
-                if round(self.Total_time, 2) == self.score_data[1]:
+                if round(self.get_total_time(), 2) == self.score_data[1]:
                     arcade.draw_text(f"2 {self.score_format} {self.score_data[1]} SEC", self.player.center_x - 350,
                                      self.player.center_y + 240, arcade.csscolor.RED, 30)
                 else:
@@ -244,7 +301,7 @@ class Chapter2View(arcade.View):
                                  self.player.center_y + 240, arcade.csscolor.BLACK, 30)
 
             try:
-                if round(self.Total_time, 2) == self.score_data[2]:
+                if round(self.get_total_time(), 2) == self.score_data[2]:
                     arcade.draw_text(f"3 {self.score_format} {self.score_data[2]} SEC", self.player.center_x - 350,
                                      self.player.center_y + 190, arcade.csscolor.BLACK, 30)
                 else:
@@ -254,17 +311,17 @@ class Chapter2View(arcade.View):
                 arcade.draw_text("No Score", self.player.center_x - 350,
                                  self.player.center_y + 190, arcade.csscolor.BLACK, 30)
 
-            if self.Total_time > self.score_data[2]:
-                arcade.draw_text(f"{linear_search(round(self.Total_time, 2), self.score_data) + 1} {self.score_format} "
-                                 f"{round(self.Total_time, 2)} SEC", self.player.center_x - 350,
+            if self.get_total_time() > self.score_data[2]:
+                arcade.draw_text(f"{linear_search(round(self.get_total_time(), 2), self.score_data) + 1} {self.score_format} "
+                                 f"{round(self.get_total_time(), 2)} SEC", self.player.center_x - 350,
                                  self.player.center_y + 140, arcade.csscolor.RED, 30)
 
         # The information text
         arcade.draw_text(self.information_text, 400, 150, arcade.csscolor.BLACK, 15)
 
         # The direction not showing time
-        if (self.Total_time - self.button_press_time) < 5:
-            self.player.create_direction(self.button_hit_time)
+        if (self.get_total_time() - self.get_button_press_time()) < 5:
+            self.player.create_direction(self.get_button_hit_time())
 
     def update(self, delta_time: float):
         self.player.check_bag()
@@ -273,28 +330,28 @@ class Chapter2View(arcade.View):
         coin_hit_list = arcade.check_for_collision_with_list(self.player, self.coin_list)
         for coin in coin_hit_list:
             coin.remove_from_sprite_lists()
-            self.player.coins += 1
-            self.player.bag.append(self.player.coins)
+            self.player.set_coins(self.player.get_coins()+1)
+            self.player.bag.append(self.player.get_coins())
 
         # Check fi the player hit the button, remove it and display the direction if so
         button_hit_list = arcade.check_for_collision_with_list(self.player, self.button_list)
         for button in button_hit_list:
-            self.button_press_time = self.Total_time
+            self.set_button_press_time(self.get_total_time())
             button.remove_from_sprite_lists()
-            self.button_hit_time += 1
+            self.set_button_hit_time(self.get_button_hit_time()+1)
 
         # Keep counting time if the game is not end
         if self.end_screen is False:
-            self.Total_time += delta_time
+            self.set_total_time(self.get_total_time()+delta_time)
 
         # Set the charge power length
         if self.up_pressed is False:
-            self.player.charge_power = 0
+            self.player.set_charge_power(0)
         else:
             if self.player.change_y == 0:
-                self.player.charge_power += 1
-                if self.player.charge_power > 69:
-                    self.player.charge_power = 70
+                self.player.set_charge_power(self.player.get_charge_power()+1)
+                if self.player.get_charge_power() > 69:
+                    self.player.set_charge_power(70)
 
         self.player.update()
         self.physics_engine.update()
@@ -302,26 +359,26 @@ class Chapter2View(arcade.View):
     def on_key_press(self, key, modifiers):
         if key == arcade.key.SPACE:
             self.up_pressed = True
-            self.player.Player_jump_speed = 3
-            self.space_press_time = self.Total_time
+            self.player.set_player_jump_speed(3)
+            self.set_space_press_time(self.get_total_time())
 
         if key == arcade.key.RIGHT:
             if self.end_screen is True:
                 self.player.change_x = 0
             else:
                 self.right_pressed = True
-                self.player.change_x += self.player.Player_move_speed
+                self.player.change_x += self.player.get_player_move_speed()
 
         if key == arcade.key.LEFT:
             if self.end_screen is True:
                 self.player.change_x = 0
             else:
                 self.left_pressed = True
-                self.player.change_x -= self.player.Player_move_speed
+                self.player.change_x -= self.player.get_player_move_speed()
 
         if self.player.collides_with_list(self.door_list) and self.player.open_door:
             if key == arcade.key.E:
-                self.player.display_score(self.Total_time)
+                self.player.display_score(self.get_total_time())
                 self.end_screen = True
 
         if key == arcade.key.ESCAPE and self.end_screen:
@@ -330,13 +387,11 @@ class Chapter2View(arcade.View):
     def on_key_release(self, key, modifiers):
         if key == arcade.key.SPACE:
             self.up_pressed = False
-            self.player.Player_jump_speed = self.player.calculate_jumpspeed(self.space_press_time, self.Total_time,
-                                                                            self.player.Player_jump_speed)
-            # Set the highest player jump speed is 15
-            if self.player.Player_jump_speed > 15:
-                self.player.Player_jump_speed = 15
+            self.player.set_player_jump_speed(self.player.calculate_jumpspeed(self.get_space_press_time(),
+                                                                              self.get_total_time(),
+                                                                              self.player.get_player_jump_speed()))
             if self.player.change_y == 0:
-                self.player.change_y += self.player.Player_jump_speed
+                self.player.change_y += self.player.get_player_jump_speed()
 
         if key == arcade.key.RIGHT:
             self.right_pressed = False
